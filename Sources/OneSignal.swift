739029475783ff0f3d174314b698a0c9716fb6b4
Khoa Pham <onmyway133@gmail.com>
2016-08-25 22:04:58 +0200

    Register users

--- Sources/OneSignal.swift
+++ Sources/OneSignal.swift
@@ -13,15 +13,56 @@ public struct OneSignal {
 
   public static var appID: String = ""
 
+  static let version = "011303"
+  static let baseURL = NSURL(string: "https://onesignal.com/api/v1")!
+
   public static func handleBadge(count: Int) {
 
   }
 
   public static func handleDeviceToken(data: NSData) {
-    
+    UserDefaults.deviceToken = Utils.parseDeviceToken(data)
   }
 
-  public static func registerUser() {
-    
+  public static func registerUser(completion: ((String?) -> Void)) {
+    let params: [String: AnyObject] = [
+      "app_id" : appID,
+      "device_model" : Utils.deviceModel(),
+      "device_os" : Utils.systemVersion(),
+      "language" : Utils.language(),
+      "timezone" : Utils.timezone(),
+      "device_type" : NSNumber(integer : 0),
+      "sounds" : Utils.soundFiles(),
+      "sdk" : version,
+      "identifier" : UserDefaults.deviceToken ?? "",
+      "net_type" : NSNumber(integer: 0),
+      "rooted": NSNumber(bool: false),
+      "as_id": "OptedOut",
+      "sdk_type": "native",
+      "ios_bundle": NSBundle.mainBundle().bundleIdentifier ?? "",
+      "notification_types": NSNumber(integer: 0)
+    ]
+
+    let url: NSURL
+
+    if let playerID = UserDefaults.playerID {
+      url = baseURL.URLByAppendingPathComponent("players/\(playerID)/on_session")
+    } else {
+      url = baseURL.URLByAppendingPathComponent("players")
+    }
+
+    Alamofire
+      .request(.POST, url, parameters: params)
+      .responseJSON { response in
+        guard let json = response.result.value as? [String: AnyObject],
+          id = json["id"] as? String
+          else {
+            completion(nil)
+            return
+        }
+
+        UserDefaults.playerID = id
+        completion(id)
+      }
   }
 }
--- Sources/Utils.swift
+++ Sources/Utils.swift
@@ -20,4 +20,44 @@ struct Utils {
 
     return string
   }
+
+  static func deviceModel() -> String {
+    var systemInfo = utsname()
+    uname(&systemInfo)
+    var v = systemInfo.machine
+
+    var deviceModel = ""
+    let _ = withUnsafePointer(&v) {
+      deviceModel = String(UTF8String: UnsafePointer($0)) ?? ""
+    }
+
+    return deviceModel
+  }
+
+  static func systemVersion() -> String {
+    let version = NSProcessInfo.processInfo().operatingSystemVersion
+
+    return "\(version.majorVersion).\(version.minorVersion).\(version.patchVersion)"
+  }
+
+  static func language() -> String {
+    return NSLocale.preferredLanguages().first!
+  }
+
+  static func timezone() -> NSNumber {
+    return NSNumber(integer: NSTimeZone.localTimeZone().secondsFromGMT)
+  }
+
+  static func soundFiles() -> [String] {
+    guard let resourcePath = NSBundle.mainBundle().resourcePath
+      else { return [] }
+
+    let files = try? NSFileManager.defaultManager()
+      .contentsOfDirectoryAtPath(resourcePath)
+      .filter {
+        return $0.hasSuffix(".wav") || $0.hasSuffix(".mp3")
+      }
+
+    return files ?? []
+  }
 }
