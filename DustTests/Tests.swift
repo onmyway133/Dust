76c05ff30ff2c54aa20153889b1c4aefb9479d64
Khoa Pham <onmyway133@gmail.com>
2016-08-25 22:05:08 +0200

    Update tests

--- DustTests/Tests.swift
+++ DustTests/Tests.swift
@@ -7,29 +7,25 @@
 //
 
 import XCTest
+import Dust
 
 class Tests: XCTestCase {
 
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+  override func setUp() {
+    super.setUp()
+
+    OneSignal.appID = "78610b70-edc6-4c61-ad5d-45083b6d3361"
+  }
+
+  func testRegister() {
+    let expectation = self.expectationWithDescription("test")
+
+    OneSignal.registerUser() { playerID in
+      XCTAssertNotNil(playerID)
+      expectation.fulfill()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    self.waitForExpectationsWithTimeout(5.0, handler: nil)
+  }
 
 }
